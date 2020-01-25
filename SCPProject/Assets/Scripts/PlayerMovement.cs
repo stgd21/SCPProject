3af7838252c4088b7960f305e094e73a14718968
stgd21 <45217325+stgd21@users.noreply.github.com>
2020-01-25 15:13:30 -0700

    Project Upload

---
+++ SCPProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public float mouseSensitivity;
+    public float moveSpeed;
+    public float maxVelocity;
+
+    private Rigidbody playerRigid;
+    private float xAxisClamp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRigid = GetComponent<Rigidbody>();
+
+        //Keep mouse invisible and locked to center of play
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateCameraRotation();
+        UpdateMovement();
+
+        //Unlock mouse
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    void UpdateMovement()
+    {
+        //Physics-based movement, Inputs from 0 to 1 based on bindings in Unity window
+        //Combines forward and horizontal movement
+        playerRigid.AddForce(transform.forward * Input.GetAxis("Vertical") * moveSpeed
+            + transform.right * Input.GetAxis("Horizontal") * moveSpeed, ForceMode.VelocityChange);
+
+        //Clamp max speed
+        if (playerRigid.velocity.magnitude > maxVelocity)
+        {
+            playerRigid.velocity = playerRigid.velocity.normalized;
+            playerRigid.velocity *= maxVelocity;
+        }
+    }
+
+    //Not my original code
+    void UpdateCameraRotation()
+    {
+        //Get the change in mouse movement per frame
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        //Make it significant and preferable
+        float rotAmountX = mouseX * mouseSensitivity;
+        float rotAmountY = mouseY * mouseSensitivity;
+
+        xAxisClamp -= rotAmountY;
+
+        //Camera will rotate vertically
+        Vector3 targetRotCam = playerCamera.transform.rotation.eulerAngles;
+        //Player will rotate horizontally with physics
+        Vector3 targetRotBody = playerRigid.rotation.eulerAngles;
+
+        //While I don't understand this math, these lines of code agree not to hurt me if I keep them in
+        targetRotCam.x -= rotAmountY;
+        targetRotCam.z = 0;
+        targetRotBody.y += rotAmountX;
+
+        if (xAxisClamp > 90)
+        {
+            xAxisClamp = 90;
+            targetRotCam.x = 90;
+        }
+        else if (xAxisClamp < -90)
+        {
+            xAxisClamp = -90;
+            targetRotCam.x = 270;
+        }
+
+        //Rotate the camera (up and down)
+        playerCamera.transform.rotation = Quaternion.Euler(targetRotCam);
+        //Rotate the player (left and right)
+        playerRigid.rotation = Quaternion.Euler(targetRotBody);
+    }
+}
---
+++ SCPProject/Assets/Scripts/SCP173.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Important
+using UnityEngine.AI;
+
+public class SCP173 : MonoBehaviour
+{
+    //Using the camera component this time rather than full gameobject
+    public Camera cam;
+
+    private BoxCollider objCollider;
+    private Plane[] planes;
+    private bool moveable = false;
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        
+        objCollider = GetComponent<BoxCollider>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    bool CalculateFrustumMoveability()
+    {
+        //Find camera frustum
+        planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        //If this gameobject is NOT inside the camera frustum
+        if (GeometryUtility.TestPlanesAABB(planes, objCollider.bounds) == false)
+            //Move to player
+            return true;
+        else
+            //We must still check if the player is occluded by walls
+            return CalculateIfOccluded();
+    }
+
+    bool CalculateIfOccluded()
+    {
+        //Stores raycast data
+        RaycastHit hit;
+        //Direction to shoot ray is from transform point of this gameobject to the camera's transform
+        Vector3 rayDirection = cam.gameObject.transform.position - transform.position;
+        //IMPORTANT: make sure player body does not interfere with raycasts!
+        if (Physics.Raycast(transform.position, rayDirection, out hit))
+        {
+            if (hit.transform.root.name != "PlayerBody")
+                //Allow movement is player is occluded
+                return true;
+            else
+                //In this case, the player sees this and is not occluded
+                return false;
+        }
+        return false;
+    }
+
+    void Update()
+    {
+        //Determine if we can move
+        moveable = CalculateFrustumMoveability();
+
+        if (moveable)
+            agent.SetDestination(cam.transform.position);
+        else
+            agent.SetDestination(transform.position);
+    }
+}
